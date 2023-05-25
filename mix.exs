88eeba370fe337fe6064ab0c9a444957d91965ba
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-05-25 11:52:00 -0400

    feat (WIP): Use the http_stage library to replace a local implementation
    For now we are pointing at a local version for testing.

--- mix.exs
+++ mix.exs
@@ -61,11 +61,11 @@ defmodule AlertsViewer.MixProject do
       {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.16", only: :test},
       {:httpoison, "~> 2.1"},
-      {:stream_data, "~> 0.5.0", only: :test},
       # Specify a reference until https://github.com/msharp/elixir-statistics/pull/28 is included in a version number
       {:statistics, "~> 0.6.2",
        git: "https://github.com/msharp/elixir-statistics", ref: "897851f"},
-      {:csv, "~> 3.0"}
+      {:csv, "~> 3.0"},
+      {:http_stage, path: "/Users/mshanley/dev/http_stage"}
     ]
   end
 
