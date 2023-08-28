e0b2a50c7025b7043361db875c63bfa264c43b58
Erin Moore <erinmoorethefirst@gmail.com>
2023-08-28 09:14:19 -0400

    refactor table

--- lib/alerts_viewer_web/components/core_components.ex
+++ lib/alerts_viewer_web/components/core_components.ex
@@ -529,96 +529,14 @@ defmodule AlertsViewerWeb.CoreComponents do
   """
   attr(:id, :string, required: true)
   attr(:rows, :list, required: true)
-  attr(:row_id, :any, default: nil, doc: "the function for generating the row id")
   attr(:row_click, :any, default: nil, doc: "the function for handling phx-click on each row")
   attr(:sticky, :boolean, default: false, doc: "set to true to make table header sticky")
 
-  attr(:row_item, :any,
-    default: &Function.identity/1,
-    doc: "the function for mapping each row before calling the :col and :action slots"
+  attr(:stream, :boolean,
+    default: false,
+    required: false,
+    doc: "whether the table is made using livestreams"
   )
-
-  slot :col, required: true do
-    attr(:label, :string)
-    attr(:title, :string, required: false)
-  end
-
-  slot(:action, doc: "the slot for showing user actions in the last table column")
-
-  def stream_table(assigns) do
-    # assigns =
-    #   with %{rows: %Phoenix.LiveView.LiveStream{}} <- assigns do
-    #     assign(assigns, row_id: assigns.row_id || fn {id, _item} -> id end)
-    #   end
-
-    ~H"""
-    <div class="overflow-y-auto px-4 sm:overflow-visible sm:px-0">
-      <table class="mt-11 w-[40rem] sm:w-full">
-        <thead class={"text-left text-[0.8125rem] leading-6 text-zinc-500 z-10 #{if(@sticky, do: ~s(sticky top-0  bg-white), else: ~s())}"}>
-          <tr>
-            <th :for={col <- @col} class="p-0 pb-4 pr-6 font-normal" title={Map.get(col, :title, "")}>
-              <%= col[:label] %>
-            </th>
-            <th class="relative p-0 pb-4"><span class="sr-only"><%= gettext("Actions") %></span></th>
-          </tr>
-        </thead>
-        <tbody
-          id={@id}
-          phx-update="stream"
-          class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
-        >
-          <tr :for={{dom_id, row} <- @rows} id={dom_id} class="group hover:bg-zinc-50">
-            <td
-              :for={{col, i} <- Enum.with_index(@col)}
-              phx-click={@row_click && @row_click.(row)}
-              class={["relative p-0", @row_click && "hover:cursor-pointer"]}
-            >
-              <div class="block py-4 pr-6">
-                <span class="absolute -inset-y-px right-0 -left-4 group-hover:bg-zinc-50 sm:rounded-l-xl" />
-                <span class={[
-                  "relative",
-                  i == 0 && "font-semibold text-zinc-900",
-                  Map.get(row, :row_class, "")
-                ]}>
-                  <%= render_slot(col, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-            <td :if={@action != []} class="relative p-0 w-14">
-              <div class="relative whitespace-nowrap py-4 text-right text-sm font-medium">
-                <span class="absolute -inset-y-px -right-4 left-0 group-hover:bg-zinc-50 sm:rounded-r-xl" />
-                <span
-                  :for={action <- @action}
-                  class="relative ml-4 font-semibold leading-6 text-zinc-900 hover:text-zinc-700"
-                >
-                  <%= render_slot(action, @row_item.(row)) %>
-                </span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    """
-  end
-
-  @doc ~S"""
-  Renders a table with generic styling.
-  You can style rows by giving the row map a :row_class key with the classes as the value
-  Eg: rows |> Enum.map(& Map.put(&1, :row_class, "text-fuchsia-500 bg-orange-300"))
-
-  ## Examples
-
-      <.table id="users" rows={@users}>
-        <:col :let={user} label="id" title="hello world"><%= user.id %></:col>
-        <:col :let={user} label="username"><%= user.username %></:col>
-      </.table>
-  """
-  attr(:id, :string, required: true)
-  attr(:rows, :list, required: true)
-  attr(:row_id, :any, default: nil, doc: "the function for generating the row id")
-  attr(:row_click, :any, default: nil, doc: "the function for handling phx-click on each row")
-  attr(:sticky, :boolean, default: false, doc: "set to true to make table header sticky")
 
   attr(:row_item, :any,
     default: &Function.identity/1,
@@ -633,9 +551,11 @@ defmodule AlertsViewerWeb.CoreComponents do
   slot(:action, doc: "the slot for showing user actions in the last table column")
 
   def table(assigns) do
+    # Make non-stream rows same shape as stream rows
     assigns =
-      with %{rows: %Phoenix.LiveView.LiveStream{}} <- assigns do
-        assign(assigns, row_id: assigns.row_id || fn {id, _item} -> id end)
+      case assigns.stream do
+        true -> assigns
+        false -> assign(assigns, :rows, Enum.map(assigns.rows, fn row -> {nil, row} end))
       end
 
     ~H"""
@@ -654,7 +574,7 @@ defmodule AlertsViewerWeb.CoreComponents do
           phx-update={match?(%Phoenix.LiveView.LiveStream{}, @rows) && "stream"}
           class="relative divide-y divide-zinc-100 border-t border-zinc-200 text-sm leading-6 text-zinc-700"
         >
-          <tr :for={row <- @rows} id={@row_id && @row_id.(row)} class="group hover:bg-zinc-50">
+          <tr :for={{dom_id, row} <- @rows} id={dom_id} class="group hover:bg-zinc-50">
             <td
               :for={{col, i} <- Enum.with_index(@col)}
               phx-click={@row_click && @row_click.(row)}
